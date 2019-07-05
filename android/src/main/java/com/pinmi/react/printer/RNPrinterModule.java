528d6c058ab988d745f16de1eeec5fe041fcc3da
Yogi Eka <yogieeka@gmail.com>
2019-07-05 11:01:49 +0700

    1st commit

---
+++ android/src/main/java/com/pinmi/react/printer/RNPrinterModule.java
@@ -0,0 +1,90 @@
+package com.pinmi.react.printer;
+
+
+import android.hardware.usb.UsbDevice;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.pinmi.react.printer.adapter.USBPrinterAdapter;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+/**
+ * Created by xiesubin on 2017/9/21.
+ */
+
+public class RNPrinterModule extends ReactContextBaseJavaModule {
+
+
+    private USBPrinterAdapter adapter;
+
+    public RNPrinterModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.adapter = USBPrinterAdapter.getInstance();
+        this.adapter.init(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "RNPrinter";
+    }
+
+
+    @ReactMethod
+    public void getUSBDeviceList(Promise promise) {
+        List<UsbDevice> usbDevices = adapter.getDeviceList();
+        WritableArray pairedDeviceList = Arguments.createArray();
+        for (UsbDevice usbDevice : usbDevices) {
+            WritableMap deviceMap = Arguments.createMap();
+            deviceMap.putString("device_name", usbDevice.getDeviceName());
+            deviceMap.putInt("device_id", usbDevice.getDeviceId());
+            deviceMap.putInt("vendor_id", usbDevice.getVendorId());
+            deviceMap.putInt("product_id", usbDevice.getProductId());
+            pairedDeviceList.pushMap(deviceMap);
+        }
+        promise.resolve(pairedDeviceList);
+    }
+
+
+    @ReactMethod
+    public void connectPrinter(Integer vendorId, Integer productId, Promise promise) {
+        if (!adapter.selectDevice(vendorId, productId)) {
+            promise.resolve(false);
+        } else {
+            promise.resolve(true);
+        }
+    }
+
+
+    @ReactMethod
+    public void closeConn(Promise promise) {
+        adapter.closeConnectionIfExists();
+        promise.resolve(null);
+    }
+
+
+    @ReactMethod
+    public void printText(String text) {
+        adapter.printText(text);
+    }
+
+    @ReactMethod
+    public void printRawData(String base64Data) {
+        adapter.printRawData(base64Data);
+    }
+
+    @ReactMethod
+    public void printPic(String base64Data, ReadableMap options) {
+        adapter.printPic(base64Data, options);
+    }
+
+}
+
